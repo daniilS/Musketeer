8a872fb142fb11fd9346eddd5a72dc635b5ea957
daniilS <daniil.soloviev@gmail.com>
2021-11-08 11:42:12 +0000

    Add nonnegativity option to fitting

--- musketeer/fitSignals.py
+++ musketeer/fitSignals.py
@@ -1,4 +1,6 @@
+import numpy as np
 from numpy.linalg import lstsq
+from scipy.optimize import lsq_linear
 
 from . import moduleFrame
 
@@ -32,11 +34,28 @@ class FitSignals(moduleFrame.Strategy):
         return fittedSignals, residuals, fittedCurves
 
 
+class FitSignalsNonnegative(moduleFrame.Strategy):
+    def __init__(self, titration):
+        self.titration = titration
+
+    def __call__(self, signalVars):
+        fittedSignals = np.empty((0, signalVars.shape[1]))
+        residuals = np.empty((1, 0))
+        for signal in self.titration.processedData.T:
+            result = lsq_linear(signalVars, signal, (0, np.inf))
+            fittedSignals = np.vstack((fittedSignals, result.x))
+            residuals = np.append(residuals, result.cost)
+        fittedSignals = fittedSignals.T
+        fittedCurves = signalVars @ fittedSignals
+        return fittedSignals, residuals, fittedCurves
+
+
 class ModuleFrame(moduleFrame.ModuleFrame):
     frameLabel = "Fit signals"
     dropdownLabelText = "Fit signals to curve using:"
     # TODO: add least squares with linear constraints
     dropdownOptions = {
-        "Ordinary least squares": FitSignals
+        "Ordinary least squares": FitSignals,
+        "Least squares with nonnegativity constraint": FitSignalsNonnegative
     }
     attributeName = "fitSignals"
