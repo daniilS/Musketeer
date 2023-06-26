faffbf8fdd0ff7a3aa02b1bfc4c4026eff31206e
daniilS <daniil.soloviev@gmail.com>
2023-06-26 00:08:20 +0000

    Add author info to msi

--- cx_freeze/setup.py
+++ cx_freeze/setup.py
@@ -11,6 +11,7 @@ build_options = {
 msi_options = {
     "install_icon": "logo 48px.ico",
     "upgrade_code": "{C327C15B-6058-313C-AADF-E979233602A5}",
+    "summary_data": {"author": "Daniil Soloviev"},
 }
 
 
