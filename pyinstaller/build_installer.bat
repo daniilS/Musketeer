e18e08097672870ceb71655a457e0c25a87118a6
daniilS <daniil.soloviev@gmail.com>
2025-10-13 14:54:36 +0100

    Improve cleanup when building installer on Windows

--- pyinstaller/build_installer.bat
+++ pyinstaller/build_installer.bat
@@ -1,3 +1,6 @@
+if exist venv (rmdir /s /q .\venv\ || exit /b)
+if exist build (rmdir /s /q .\build\ || exit /b)
+if exist dist (rmdir /s /q .\dist\ || exit /b)
 python -m venv --clear --upgrade-deps venv || exit /b
 call venv\Scripts\activate.bat
 
@@ -6,10 +9,11 @@ pip install --upgrade --upgrade-strategy=eager wheel pyinstaller  ..
 
 pyivf-make_version --source-format distribution --metadata-source musketeer --outfile version_info.txt
 
-pyinstaller --noconfirm Musketeer.spec
+pyinstaller --noconfirm Musketeer.spec || goto cleanup
 
 for /f %%i in ('python -c "import musketeer; print(musketeer.__version__)"') do set musketeer_version=%%i
 
 "C:\Program Files (x86)\Inno Setup 6\iscc.exe" /DMyAppVersion=%musketeer_version% musketeer.iss
 
+:cleanup
 deactivate
